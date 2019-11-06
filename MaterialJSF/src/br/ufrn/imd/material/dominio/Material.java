464cc2943fcdefdd0daff00f99c505340ece1fbd
Rayane Alves <33555205+mrayanealves@users.noreply.github.com>
2019-11-06 10:06:57 -0300

    Update Material.java

--- MaterialJSF/src/br/ufrn/imd/material/dominio/Material.java
+++ MaterialJSF/src/br/ufrn/imd/material/dominio/Material.java
@@ -14,8 +14,6 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotBlank;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 @Entity
 @Table(name = "material")
 public class Material {
@@ -36,7 +34,6 @@ public class Material {
 	@JoinColumn(name = "id_usuario")
 	private Usuario usuarioCadastro;
 	
-	@JsonIgnore
 	public Integer getId() {
 		return id;
 	}
